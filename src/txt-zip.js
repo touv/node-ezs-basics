3ecd6d0f7cbe73259d7fe210bfa3b013baed14b2
François Parmentier <francois.parmentier@gmail.com>
2019-05-16 18:06:06 +0200

    :zap: Add TXTZip

---
+++ src/txt-zip.js
@@ -0,0 +1,25 @@
+import pako from 'pako';
+
+let deflator;
+function TXTZip(data, feed) {
+    if (this.isLast()) {
+        deflator.push('', true);
+        return feed.close();
+    }
+    if (this.isFirst()) {
+        deflator = new pako.Deflate({ level: 9 });
+        deflator.onData = chunk => feed.write(chunk);
+    }
+    deflator.push(data, false);
+    feed.end();
+}
+
+/**
+ * Take a String and zip it
+ *
+ * @name TXTZip
+ * @returns {Buffer}
+ */
+export default {
+    TXTZip,
+};
